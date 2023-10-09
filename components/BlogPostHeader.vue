081a49e4743880a7ca9e6ba9ebd6ab5c6c6cfae3
ArielMejiaDev <arielmejiadev@gmail.com>
2023-10-09 10:25:30 -0600

    adjust some styles

--- components/BlogPostHeader.vue
+++ components/BlogPostHeader.vue
@@ -5,15 +5,16 @@ defineProps({
 </script>
 
 <template>
-  <div class="font-bold text-sm tracking-tighter">
+  <div class="not-prose font-bold text-sm tracking-tighter">
     <div class="flex space-x-2">
       <NuxtLink :to="`/blog/categories/${tag}`" v-for="(tag, index) in data.tags" :key="index" class="not-prose p-1 cursor-pointer rounded border border-indigo-400 bg-indigo-400 text-indigo-50 font-semibold hover:bg-indigo-500">
         {{ tag }}
       </NuxtLink>
     </div>
-    <h1 class="not-prose mx-0 my-4 text-4xl font-extrabold text-left text-gray-900 capitalize lg:my-8 lg:text-5xl lg:leading-none">{{ data.title }}</h1>
-    <div class="not-prose flex items-center space-x-4">
-      <img src="/images/avatar.png" alt="Ariel Mejia Illustration" class="h-20 w-20 rounded-full border border-purple-500 self-start" />
+    <h1 class="mx-0 my-4 text-3xl sm:text-4xl font-black text-left text-gray-900 capitalize lg:my-8 lg:text-6xl lg:leading-none">
+      {{ data.title }}
+    </h1>
+    <div class="flex items-center space-x-4">
       <div class="flex flex-col">
         <p class="text-2xl font-black tracking-tight leading-tight">Ariel Mejia</p>
         <div class="not-prose flex space-x-4">
