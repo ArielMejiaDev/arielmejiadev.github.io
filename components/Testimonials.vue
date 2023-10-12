73d0a87ab858f2a21b269b318892d926283a1175
ArielMejiaDev <arielmejiadev@gmail.com>
2023-10-11 19:11:00 -0600

    remove js imports to avoid static site build issues

--- components/Testimonials.vue
+++ components/Testimonials.vue
@@ -1,5 +1,36 @@
 <script setup>
-import testimonials from "../content/scripts/testimonials";
+const testimonials = [
+  {
+    avatar_url: 'irene.jpeg',
+    name: 'Irene Valdes Salazar',
+    comment: `I worked with Ariel ... he was my mentor and advisor for a project, He is very knowledgable regarding the latest trends in technology... he is passionate about what he does and it comes through in his work.`,
+    position: 'Intern - Apple'
+  },
+  {
+    avatar_url: 'christian.jpeg',
+    name: 'Christian Saravia',
+    comment: `Ariel is very passionate about his work, and is particularly excellent at PHP & Laravel programming. He spends a lot of his free time learning new skills, which translates into real results at work. He's also a very good person with good values, which helps make a positive impact in a company's culture.`,
+    position: 'CEO & Founder - Buildawow'
+  },
+  {
+    avatar_url: 'adrian.jpeg',
+    name: 'Adrian Catalan',
+    comment: `His skills as full stack developer includes PHP & Laravel (Backend), VueJS (Front-end) and Flutter (mobile) and he can easily adapt to learn new languages, frameworks and tools as needed ... His community work shows strengths on planning, organizing and executing projects beyond code.`,
+    position: 'Google Developer Expert'
+  },
+  {
+    avatar_url: 'alex.jpeg',
+    name: 'Alex Stockman',
+    comment: `It was an absolute pleasure working with Ariel. He’s particularly reliable!`,
+    position: 'Growth Manager - Firstbase.io'
+  },
+  {
+    avatar_url: 'steven.jpeg',
+    name: 'Steven Klose',
+    comment: `I had the pleasure of working with Ariel in a turnaround project in a fast changing environment. Not only did he change directions swiftly... deadlines where surpassed. An absolute pleasure to work with Ariel!`,
+    position: 'Founder - SYNQ'
+  },
+];
 
 const randomTestimonials = ref([]);
 
